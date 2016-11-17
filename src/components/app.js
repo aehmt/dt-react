b3b5cddd5f11cb51bb42e5f3179038c6e52d36dd
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-11-17 17:57:51 -0500

    nothing works

--- src/components/app.js
+++ src/components/app.js
@@ -6,20 +6,30 @@ const socket = io();
 export default class App extends React.Component {
   constructor(props){
     super(props);
-    let room = props.room;
-
     this.state = {
       shapes: [],
-      color: '#000000'
+      color: '#000000',
+      startingData: null
     }
-
-    socket.on('connect', function() {
-      console.log('Connected!');
-      socket.emit("subscribe", {room: room});
-    })
     socket.on('draw', (newDrawState) => this.handleStateChange(newDrawState));
+    let room = this.props.room;
+    socket.on('connect', function() {
+      socket.emit("subscribe", {room: room})
+    })
     this.handleClick = this.handleClick.bind(this)
     this.handleColorChange = this.handleColorChange.bind(this)
+    this.getLoad = this.getLoad.bind(this)
+    if (this.state.startingData) {
+      socket.on('geteverything', console.log('hello'))
+    }
+    socket.on('loadstuff', (data) => console.log(data))
+  }
+  componentDidMount() {
+      let loadedImage = new Image()
+      loadedImage.src = this.state.startingData
+      let canvas = document.getElementById('ourCanvas');
+      let ctx = canvas.getContext('2d');
+      ctx.drawImage(loadedImage, 0, 0) 
   }
   handleStateChange(newDrawState) {
     let shapes = [...this.state.shapes, newDrawState]
@@ -30,6 +40,19 @@ export default class App extends React.Component {
       shapes
     })
   }
+  sendLoad(){
+    console.log('hello from sendload')
+    let canvas = document.getElementById('ourCanvas');
+    let imgData = canvas.toDataURL()
+    console.log(imgData)
+    socket.emit('loadstuff', imgData)
+  }
+  getLoad(imgData){
+    console.log(imgData)
+    this.setState({
+      startingData: imgData
+    })
+  } 
   handleClick(ev){
     ev.preventDefault();
     socket.emit('draw', [ev.pageX, ev.pageY-75, this.state.color]);
