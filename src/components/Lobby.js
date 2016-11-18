bef1fecc037525cb495ac13f4fe7b81e420612cf
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-11-18 13:14:05 -0500

    why isnt this working anymore

---
+++ src/components/Lobby.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'react-router'
+
+
+const Lobby = () => {
+  return (
+    <div>
+      Available rooms:
+    </div>
+  )
+}
+
+export default Lobby
--- src/components/SaveButton.js
+++ src/components/SaveButton.js
@@ -6,9 +6,7 @@ export default class SaveButton extends React.Component {
     let imgdata = canvas.toDataURL()
     let data = {'artwork': {'editable': true, 'state': imgdata}}
     console.log(data)
-    fetch('https://dry-fortress-11373.herokuapp.com/api/v1/artworks', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)})
-      .then(res => res.json())
-      .then(json => console.log(res))
+    fetch('https://dry-fortress-11373.herokuapp.com/api/v1/artworks', {method: 'PATCH', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)})
   } 
   render(){
     return (
