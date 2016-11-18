0ea0b85aac6cdbb99f7c9ebf0d79262a793a4d24
aehmt <calisahmet@gmail.com>
2016-11-18 17:42:03 -0500

    squares added

--- src/components/canvasComponent.js
+++ src/components/canvasComponent.js
@@ -1,28 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import DefaultShape from './defaultShape'
 import DefaultShapeDelete from './defaultShapeDelete'
 import fetch from 'isomorphic-fetch'
 
-const CanvasComponent = (props) => {
-  let rendered;
-    if (props.shapes.length <= 20) {
-      rendered = props.shapes.map((shape, i) => <DefaultShape xCo={shape[0]} yCo={shape[1]} key={i}/>)
+class CanvasComponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+    
+  
+  render() {
+    var rendered;
+    if (this.props.shapes.length <= 20) {
+      rendered = this.props.shapes.map((shape, i) => <DefaultShape xCo={shape[0]} yCo={shape[1]} color={shape[2]} key={i} pickedShape={this.props.pickedShape}/>)
     } else {
-      rendered = props.shapes.map((shape, i) => {
-        if (i < props.shapes.length - 20) {
-          return <DefaultShape xCo={shape[0]} yCo={shape[1]} key={i} color={shape[2]} />
+      rendered = this.props.shapes.map((shape, i) => {
+        if (i < this.props.shapes.length - 20) {
+          return <DefaultShape xCo={shape[0]} yCo={shape[1]} key={i} color={shape[2]} pickedShape={this.props.pickedShape} />
         } else {
-          return <DefaultShapeDelete xCo={shape[0]} yCo={shape[1]} key={i}/>
+          return <DefaultShapeDelete xCo={shape[0]} yCo={shape[1]} key={i} pickedShape={this.props.pickedShape}/>
         }
       })
     }
     return (
       <div id="painting">
-        <canvas onMouseMove={props.onMove} id="ourCanvas" width="700" height="400" style={{backgroundColor: '#008811', borderRadius: 10, borderWidth: 0.5, borderColor: '#d6d7da', opacity: '0.75'}}>
+        <canvas onMouseMove={this.props.onMove} id="ourCanvas" width="700" height="400" style={{backgroundColor: '#AAAAAA', borderRadius: 10, borderWidth: 0.5, borderColor: '#d6d7da'}}>
           {rendered}
         </canvas>
       </div>
     )
   }
+}
 
-export default CanvasComponent
+export default CanvasComponent;
+
+
+
