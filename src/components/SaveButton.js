478c664dc854c320d6e081452d56b387d33c2044
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-11-18 11:15:34 -0500

    its then not done

--- src/components/SaveButton.js
+++ src/components/SaveButton.js
@@ -6,8 +6,8 @@ export default class SaveButton extends React.Component {
     let imgdata = canvas.toDataURL()
     let data = {'artwork': {'editable': true, 'state': imgdata}}
     fetch('https://dry-fortress-11373.herokuapp.com/api/v1/artworks', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)})
-      .done((res) => res.json())
-      .done((json) => console.log(res))  
+      .then((res) => res.json())
+      .then((json) => console.log(res))  
   } 
   render(){
     return (
