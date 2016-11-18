05185980d96784b1808889eeecdf3ff8f51fad12
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-11-18 16:36:56 -0500

    so close to choosing which to load on canvas

--- src/components/SaveButton.js
+++ src/components/SaveButton.js
@@ -5,7 +5,6 @@ export default class SaveButton extends React.Component {
     let canvas = document.getElementById('ourCanvas');
     let imgdata = canvas.toDataURL()
     let data = {'artwork': {'editable': true, 'state': imgdata}}
-    console.log(data)
     fetch('https://dry-fortress-11373.herokuapp.com/api/v1/artworks', {method: 'PATCH', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)})
   } 
   render(){
