3cd88b84a596728cfe12c4b9e6f08e00c63fc3a0
satub <satu.barnhill@gmail.com>
2016-11-18 15:02:33 -0500

    working room index

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -1,13 +1,27 @@
 import React from 'react'
-import Link from 'react-router'
+import {Link} from 'react-router'
+const BASE_URL = 'https://dry-fortress-11373.herokuapp.com/api/v1/'
 
 
-const Lobby = () => {
-  return (
-    <div>
-      Available rooms:
-    </div>
-  )
+export default class Lobby extends React.Component {
+  constructor(){
+    super()
+    this.state = {rooms: []}
+  }
+  componentWillMount(){
+    fetch(`${BASE_URL}artworks`)
+      .then(res => res.json())
+      .then(json => this.setState({
+          rooms: json.artworks.map(artwork => artwork.id)
+        })
+      )
+    }
+
+  render(){
+    return (
+      <div>
+        Available rooms: {this.state.rooms.map(room => (<Link key={room} to={`/rooms/${room}`}>Room {room}</Link>))}
+      </div>
+    )
+  }
 }
-
-export default Lobby
