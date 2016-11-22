a38d189e36ed24084fd14b293adbf2f0495799bf
aehmt <calisahmet@gmail.com>
2016-11-22 11:14:11 -0500

    console.log deleted

--- src/components/canvasComponent.js
+++ src/components/canvasComponent.js
@@ -6,7 +6,6 @@ import fetch from 'isomorphic-fetch'
 const CanvasComponent = (props) => { 
     
     var rendered;
-    console.log(props.shapes)
     if (props.shapes.length <= 20) {
       rendered = props.shapes.map((shape, i) => <DefaultShape xCo={shape[0]} yCo={shape[1]} color={shape[2]} key={i} pickedShape={shape[3]}/>)
     } 
